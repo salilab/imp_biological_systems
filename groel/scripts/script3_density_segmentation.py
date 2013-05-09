5f4ac56dffa4b4ea3f79db46824f98fbbed36b33
Ben Webb <ben@salilab.org>
2013-05-09 11:51:16 -0700

    Simplify script, and remove environment variables.

--- groel/scripts/script3_density_segmentation.py
+++ groel/scripts/script3_density_segmentation.py
@@ -1,14 +1,9 @@
-import os
-density_map="data/em_maps/groel-11.5A.mrc"
-spacing=2.7
-origin=[-135,-135,-135]
-num_segments=14
-density_threshold=0.7
-centers_output="output/groel_segments_center.pdb"
-segments_output= "output/groel_subunit"
-input=[density_map,num_segments,density_threshold,centers_output,"--seg",segments_output,"--apix",spacing,"--x",origin[0],"--y",origin[1],"--z",origin[2]]
-exe="$IMP/modules/multifit/bin/density2anchors"
-command="$IMP/tools/imppy.sh "+exe+" "+str(input).replace("[","").replace("]","").replace(",", " ").replace("'","")
+import subprocess
+
+cmd = ['density2anchors', "data/em_maps/groel-11.5A.mrc",
+       "14", "0.7", "output/groel_segments_center.pdb",
+       "--seg", "output/groel_subunit", "--apix", "2.7",
+       "--x", "-135", "--y", "-135", "--z", "-135"]
 print "======running command:"
-print command
-os.system(command)
+print " ".join(cmd)
+subprocess.call(cmd)
