9e1b3aba4ca2b450ba096699d79548cc50cf8bdd
Ben Webb <ben@salilab.org>
2013-05-23 16:09:56 -0700

    Use MultiFit application 'segment' function.

--- groel/scripts/script3_density_segmentation.py
+++ groel/scripts/script3_density_segmentation.py
@@ -1,6 +1,6 @@
 import subprocess
 
-cmd = ['density2anchors', "data/em_maps/groel-11.5A.mrc",
+cmd = ['multifit.py', 'segment', "data/em_maps/groel-11.5A.mrc",
        "14", "0.7", "output/groel_segments_center.pdb",
        "--seg", "output/groel_subunit", "--apix", "2.7",
        "--x", "-135", "--y", "-135", "--z", "-135"]
