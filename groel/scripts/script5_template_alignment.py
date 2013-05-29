8e0daa1ba4df1e4fb895a3402a0a494bd4f270d7
Ben Webb <ben@salilab.org>
2013-05-29 16:20:28 -0700

    Speed up align2d().

--- groel/scripts/script5_template_alignment.py
+++ groel/scripts/script5_template_alignment.py
@@ -9,5 +9,5 @@ env.io.atom_files_directory = ['.', 'data/templates']
 mdl = model(env, file='1iok', model_segment=('FIRST:A','LAST:A'))
 aln.append_model(mdl, align_codes='1iok', atom_files='1iok.pdb')
 aln.append(file='data/sequences/groel_ecoli.ali', align_codes='P0A6F5')
-aln.align2d()
+aln.align2d(max_gap_length=50)
 aln.write(file='groel-1iokA.ali', alignment_format='PIR')
