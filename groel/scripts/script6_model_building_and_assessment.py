a95f7ad6c4d489bda0dd9cc0f089193584161592
Ben Webb <ben@salilab.org>
2013-06-06 11:47:53 -0700

    Put all outputs in 'output' directory.

--- groel/scripts/script6_model_building_and_assessment.py
+++ groel/scripts/script6_model_building_and_assessment.py
@@ -3,16 +3,19 @@
 from modeller import *
 from modeller.automodel import *
 from modeller.scripts import complete_pdb
+import os
 import IMP.em
 import IMP.multifit
 
+os.chdir('output')
+
 env = environ()
-env.io.atom_files_directory = ['./', 'data/templates']
+env.io.atom_files_directory = ['../data/templates']
 env.libs.topology.read(file='$(LIB)/top_heav.lib')
 env.libs.parameters.read(file='$(LIB)/par.lib')
 a = automodel(env, alnfile='groel-1iokA.ali',
-                            knowns='1iok', sequence='P0A6F5',
-                            assess_methods=(assess.DOPE))
+              knowns='1iok', sequence='P0A6F5',
+              assess_methods=assess.DOPE)
 a.starting_model = 1
 a.ending_model = 10
 a.make()
@@ -42,7 +45,7 @@ for mdl_fn in truncated_models_fn:
     norm_dope_scores.append(mdl.assess_normalized_dope())
 
 # score models by em
-dmap=IMP.em.read_map("output/groel_subunit_11.mrc",IMP.em.MRCReaderWriter())
+dmap=IMP.em.read_map("groel_subunit_11.mrc",IMP.em.MRCReaderWriter())
 dmap.get_header_writable().set_resolution(11.5)
 imp_mdl=IMP.Model()
 rb_refiner=IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits())
@@ -65,13 +68,14 @@ for mdl_fn in truncated_models_fn:
                               sols.get_score(0))
     #write the fitted model
     IMP.core.transform(rb,refined_sols.get_transformation(0))
-    IMP.atom.write_pdb(IMP.atom.Hierarchy(rb),"output/"+mdl_fn.split(".pdb")[0]+".fitted.pdb")
+    IMP.atom.write_pdb(IMP.atom.Hierarchy(rb),
+                       mdl_fn.split(".pdb")[0]+".fitted.pdb")
     IMP.core.transform(rb,refined_sols.get_transformation(0).get_inverse())
     IMP.core.transform(rb,sols.get_transformation(0).get_inverse())
     fitting_scores.append(1.-refined_sols.get_score(0))
 
 #print model names and their scores into a output file
-output=open("output/model_building.scores.output","w")
+output=open("model_building.scores.output","w")
 output.write('%(a)-40s%(b)-25s%(c)-25s%(d)-25s\n'%{'a':"name",'b':"full model norm_dope",'c':"truncated model norm_dope",'d':"cc"})
 for i ,mdl_fn in enumerate(truncated_models_fn):
     output.write('%(a)-40s%(b)-25.3f%(c)-25.3f%(d)-25.3f\n'%{'a':mdl_fn,'b':full_model_norm_dope_scores[i],'c':norm_dope_scores[i],'d':fitting_scores[i]})
