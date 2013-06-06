1d3277253eb6ff79c862b16ab22c07e407f926fa
Ben Webb <ben@salilab.org>
2013-06-06 15:12:09 -0700

    Test scripts 2 and 3.

--- groel/test/test.py
+++ groel/test/test.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import shutil
 import os
+import glob
 import subprocess
 import modeller
 
@@ -13,7 +14,7 @@ class Tests(unittest.TestCase):
         os.mkdir('output')
 
     def test_script1(self):
-        """Test step 1 (build_profile)"""
+        """Test step 1 (build profile)"""
         # Make sure the script runs without errors
         p = subprocess.check_call(['scripts/script1_build_profile.py'])
         # Make sure the profile contains the sequences we expect
@@ -25,6 +26,32 @@ class Tests(unittest.TestCase):
                           '1we3A', '3kfbA', '3kfeA', 'P0A6F5'])
         os.unlink('output/build_profile.prf')
         os.unlink('output/build_profile.ali')
+
+    def test_script2(self):
+        """Test step 2 (compare templates)"""
+        # Get inputs (outputs from step 1)
+        shutil.copy('precalculate_results/stage1_template_identification/' \
+                    'build_profile.prf', 'output')
+        # Make sure the script runs without errors
+        p = subprocess.check_call(['scripts/script2_compare_templates.py'])
+        os.unlink('output/family.mat')
+
+    def test_script3(self):
+        """Test step 3 (density segmentation)"""
+        # Make sure the script runs without errors
+        p = subprocess.check_call(['scripts/script3_density_segmentation.py'])
+        # Should have produced a PDB with coordinates of all 14 subunit centers
+        e = modeller.environ()
+        m = modeller.model(e, file='output/groel_segments_center.pdb')
+        self.assertEqual(len(m.atoms), 14)
+        self.assertEqual(len(m.residues), 14)
+        # load_configuration file should load all 14 subunits, and set level
+        wc = len(open('output/load_configuration.cmd').readlines())
+        self.assertEqual(wc, 15)
+        os.unlink('output/load_configuration.cmd')
+        os.unlink('output/groel_segments_center.pdb')
+        for i in range(14):
+            os.unlink('output/groel_subunit_%d.mrc' % i)
 
 if __name__ == '__main__':
     unittest.main()
