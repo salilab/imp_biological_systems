9c7e2d664a6c5186abd2a082ff51dfb699824bb1
Ben Webb <ben@salilab.org>
2013-06-06 11:21:15 -0700

    Put all output in 'output' directory.

--- groel/scripts/script3_density_segmentation.py
+++ groel/scripts/script3_density_segmentation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 import subprocess
+import os
 
-cmd = ['multifit.py', 'segment', "data/em_maps/groel-11.5A.mrc",
-       "14", "0.7", "output/groel_segments_center.pdb",
-       "--seg", "output/groel_subunit", "--apix", "2.7",
+print "cd output"
+os.chdir('output')
+
+cmd = ['multifit.py', 'segment', "../data/em_maps/groel-11.5A.mrc",
+       "14", "0.7", "groel_segments_center.pdb",
+       "--seg", "groel_subunit", "--apix", "2.7",
        "--x", "-135", "--y", "-135", "--z", "-135"]
 print "======running command:"
 print " ".join(cmd)
